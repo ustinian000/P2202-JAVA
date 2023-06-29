c9f69a2fd92df3fd4099e6cfa352a2cf233fc8cc
dingquanhong <62588101+dingquanhong@users.noreply.github.com>
2023-06-29 22:11:09 +0800

    userGUI update

--- src/fun/JBDC_Control.java
+++ src/fun/JBDC_Control.java
@@ -1,6 +1,11 @@
 package fun;
 import java.sql.*;
 public class JBDC_Control {
+    /**通用连接数据库函数
+     * @param user 数据库用户
+     * @param pass 数据库密码
+     * @return {@link Connection}
+     */
     public  Connection getConnection(String user ,String pass){
         String url = "jdbc:mysql://localhost:3306/Libray";
 
@@ -12,6 +17,14 @@ public class JBDC_Control {
         }
         return connection;
     }
+
+    /**
+     * 通用数据库关闭函数
+     *
+     * @param rs         rs
+     * @param statement  声明
+     * @param connection 连接
+     */
     public  void close(ResultSet rs , Statement statement,Connection connection){
 
         try {
