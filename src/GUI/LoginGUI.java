2886f2be5208847183cc83cc3b5ab629e35aac3b
dingquanhong <62588101+dingquanhong@users.noreply.github.com>
2023-06-29 14:30:35 +0800

    登录方法

--- src/GUI/LoginGUI.java
+++ src/GUI/LoginGUI.java
@@ -1,23 +1,39 @@
 package GUI;
 
 import javax.swing.*;
-
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import fun.LoginFUN;
 public class LoginGUI {
     private JPanel login;
     private JTextField PassField;
     private JPasswordField passwordField;
-    private JButton 登录Button;
+    private JButton loginButton;
     private JLabel username;
     private JLabel password;
     private JCheckBox AdminCheckBox;
     private JLabel Register;
     private JLabel Retrieve;
+    public LoginGUI(){
+        loginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(new LoginFUN().loginfun(1,"1750000000","111111")){
+                    System.out.println("登录成功");
+                }else {
+                    System.out.println("登录失败");
+                }
+            }
+        });
+    }
 
-    public static void main(String[] args) {
+
+    public static void show() {
         JFrame frame = new JFrame("LoginGUI");
         frame.setContentPane(new LoginGUI().login);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
+
     }
 }
---
+++ src/fun/LoginFUN.java
@@ -0,0 +1,15 @@
+package fun;
+
+public class LoginFUN {
+    /**
+     * 登录方法，返回是否登录成功
+     *
+     * @param type 用户类型
+     * @param user 用户名（手机号）
+     * @param pass 密码
+     * @return boolean T成功，F失败
+     */
+    public boolean loginfun(int type,String user,String pass){
+        return false;
+    }
+}
